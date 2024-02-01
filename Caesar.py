9dc923bfe71b1e86e935ca8dd8f73ee50f534008
Raaghav P <87406829+raagzz@users.noreply.github.com>
2024-02-01 17:24:39 +0530

    Updated Caesar.py

--- Caesar.py
+++ Caesar.py
@@ -4,29 +4,15 @@ choice = int(input("Enter your Choice (1/2):"))
 text = input('Enter your text: ')
 shift = int(input("Enter the no. of shift (1 - 25): "))
 
-class CaesarCipher:
+def operation(choice, text, shift):
+    cipher = ""
+    shift = (26 - shift) if choice == 2 else shift
 
-    def __init__(self, choice, text, shift):
-        self.choice = choice
-        self.text = text
-        self.shift = shift
+    for i in range(len(text)):
+        char = text[i]
+        offset = 65 if char.isupper() else 97
+        cipher += chr((ord(char) + shift - offset) % 26 + offset)
 
-    def operation(self):
-        cipher = ""
+    return cipher
 
-        if self.choice == 2:
-            self.shift = 26 - self.shift
-
-        for i in range(len(text)):
-            char = text[i]
-
-            if(char.isupper()):
-                cipher += chr((ord(char) + self.shift - 65) % 26 + 65)
-
-            else:
-                cipher += chr((ord(char) + self.shift - 97) % 26 + 97)
-    
-        return cipher
-
-caesar = CaesarCipher(choice, text, shift)
-print(caesar.operation())
+print(operation(choice, text, shift))
